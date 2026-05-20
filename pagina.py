ac71fb437767a3652bd9f9f4d77df99515140ae3
IvanRuiz-H <ivanalejandro.ruiz@alumnos.ulagos.cl>
2026-05-20 19:50:54 -0400

    git -m "delete(pagina.py): se borro pagina.py porque no se esta utilizando en el proyecto"

--- pagina.py
+++
@@ -1,9 +0,0 @@
-# Pedir dos números al usuario
-numero1 = float(input("Ingresa el primer número: "))
-numero2 = float(input("Ingresa el segundo número: "))
-
-# Calcular la suma
-suma = numero1 + numero2
-
-# Mostrar el resultado
-print("La suma es:", suma)
