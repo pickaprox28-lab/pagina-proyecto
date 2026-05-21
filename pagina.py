0180b31d1618b0b2e019e33baf5294e17e6e77e4
pick28 <pickaprox28@gmail.com>
2026-05-20 20:08:02 -0400

    Revert "se borro un read me y pagina.py"
    This reverts commit dc60ab7002975221cee92a68112897d1cfd4d73d.

---
+++ pagina.py
@@ -0,0 +1,9 @@
+# Pedir dos números al usuario
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Calcular la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma es:", suma)
